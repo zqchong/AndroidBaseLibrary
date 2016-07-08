4612acee2d75933796638bb61e443a3fbf587b7a
walfud <walfud@aliyun.com>
2016-07-08 14:42:09 +0800

    .

--- app/src/main/java/com/licaigc/example/MainActivity.java
+++ app/src/main/java/com/licaigc/example/MainActivity.java
@@ -2,15 +2,24 @@ package com.licaigc.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.licaigc.AndroidBaseLibrary;
+import com.licaigc.statistics.Statistics;
 
 public class MainActivity extends Activity {
+
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtn = (Button) findViewById(R.id.btn);
+        mBtn.setOnClickListener(v -> Statistics.reportStartupInfo(1));
 
         AndroidBaseLibrary.initialize(getApplicationContext());
+
+        mBtn.post(() -> mBtn.performClick());
     }
 }
